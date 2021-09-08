5c2444502291918e0f1a0841da98fe3be7bc5f73
Benjamin Kinard <benjaminjameskinard@gmail.com>
2021-09-08 16:18:54 -0600

    Implement Trie toString() method

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -25,4 +25,47 @@ public class Trie implements ITrie {
     public int getNodeCount() {
         return nodeCount;
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder curWord = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+
+        toStringHelper(root, curWord, output);
+
+        return output.toString();
+    }
+
+    private void toStringHelper(INode n, StringBuilder curWord, StringBuilder output) {
+        // If current node has a word count, append that word to the output followed by a newline
+        if(n.getValue() > 0) {
+            output.append(curWord.toString());
+            output.append("\n");
+        }
+
+        // Traverse trie by recursively calling helper on children of current node
+        for(int i = 0; i < n.getChildren().length; i++) {
+            INode child = n.getChildren()[i];
+            if(child != null) {
+                // Convert index into character the current node is meant to represent
+                char letter = (char)('a' + i);
+                // Append the current letter to the curWord StringBuilder
+                curWord.append(letter);
+                toStringHelper(child, curWord, output);
+                // Remove current letter from curWord before iterating to next child
+                curWord.deleteCharAt(curWord.length() - 1);
+            }
+        }
+    }
+
 }
