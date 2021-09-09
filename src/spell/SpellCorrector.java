7cc1a85ea61f2435613ea38df5489c144b761360
Benjamin Kinard <benjaminjameskinard@gmail.com>
2021-09-09 08:32:43 -0600

    Implement SpellCorrector constructor

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -3,6 +3,13 @@ package spell;
 import java.io.IOException;
 
 public class SpellCorrector implements ISpellCorrector {
+
+    private ITrie dictionary;
+
+    public SpellCorrector() {
+        dictionary = new Trie();
+    }
+
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
 
